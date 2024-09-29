1a6d2e37f5fd039e643a0afa05aa2ebd6b58063c
sushmasri1906 <sushmasri1906@gmail.com>
2024-09-29 22:06:12 +0530

    local setup

--- src/Constants/constants.tsx
+++ src/Constants/constants.tsx
@@ -1,3 +1,3 @@
 // Base URL for the API
-// export const api = "http://localhost:5000/ig";
-export const api = "https://hexvibe.onrender.com/ig";
+export const api = "http://localhost:5000/ig";
+// export const api = "https://hexvibe.onrender.com/ig";
