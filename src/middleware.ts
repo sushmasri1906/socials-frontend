4af8a042385734dd4dc65c000227d0c8040f0c07
sushmasri1906 <sushmasri1906@gmail.com>
2024-09-30 21:20:55 +0530

    bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,29 +2,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-	const { pathname } = request.nextUrl;
-	console.log(pathname);
-
-	// Exclude /login and /register
-	if (pathname === "/login" || pathname === "/register") {
-		return NextResponse.next(); // Skip middleware for these routes
-	}
-	const authHeader = request.headers.get("Authorization"); // Example: token in Authorization header
-	console.log("skhgsakhgash");
-	if (!authHeader) {
-		return NextResponse.redirect(new URL("/login", request.url));
-	}
-
 	return NextResponse.next();
 }
 
 export const config = {
 	matcher: [
-		"/profile/:path*", // Profile page and sub-routes
+		"/ig/:path*", // Profile page and sub-routes
 		"/",
-		"/posts/:path*", // Posts page and sub-routes
-		"/messages/:path*",
 		"/search/:path*",
+		"/messages/:path*",
+		"/profile/:path*",
+		"/posts/:path*",
 		"/user/:path*",
 	],
 };
