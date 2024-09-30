bc10f9147decad001799a2320707c99827c8d8fa
sushmasri1906 <sushmasri1906@gmail.com>
2024-09-30 21:24:12 +0530

    middleware bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+	console.log(request.url);
 	return NextResponse.next();
 }
 
